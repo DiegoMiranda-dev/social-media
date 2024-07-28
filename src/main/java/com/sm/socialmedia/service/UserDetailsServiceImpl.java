1c578f2ede67cfd17ac06c722bd0e6cb496f8a26
Miranda <mirandadiego864@gmail.com>
2024-07-28 01:12:09 -0600

    Añadir implementación de UserDetailsServiceImpl

---
+++ src/main/java/com/sm/socialmedia/service/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.sm.socialmedia.service;
+
+import com.sm.socialmedia.repository.UsuarioRepository;
+import com.sm.socialmedia.user.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findUserByNombreUsuario(username).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+        return User.builder()
+                .username(usuario.getNombreUsuario())
+                .password(usuario.getPasswordHash())
+                .roles("USER")
+                .build();
+    }
+
+
+
+}
