112bd8a45b3b87753b0684731096241af7a53673
Miranda <mirandadiego864@gmail.com>
2024-07-28 01:11:38 -0600

    Añadir manejo de errores

---
+++ src/main/java/com/sm/socialmedia/infra/errors/TratarErrores.java
@@ -0,0 +1,26 @@
+package com.sm.socialmedia.infra.errors;
+
+import com.sm.socialmedia.response.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+public class TratarErrores {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity tratarErroresDeValidacion(ConstraintViolationException e) {
+        Set<ConstraintViolation<?>> violaciones = e.getConstraintViolations();
+        List<DatosErrorValidacion> errs = violaciones.stream()
+                .map(v -> new DatosErrorValidacion(v.getMessage()))
+                .collect(Collectors.toList());
+        ApiResponse errores = new ApiResponse(false, null, errs);
+        return ResponseEntity.badRequest().body(errores);
+
+    }
+}
